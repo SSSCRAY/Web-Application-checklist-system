8915e10150b20c6d6ee7239e0f605d1ab0270d81
artem <artemcherepanov551@gmail.com>
2026-02-28 15:42:21 +0500

    fix: production API url

--- src/main/resources/static/api.js
+++ src/main/resources/static/api.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:8080';
+const API = 'https://web-application-checklist-system-production.up.railway.app';
 
 function getToken() { return localStorage.getItem('token'); }
 function getRole()  { return localStorage.getItem('role'); }
