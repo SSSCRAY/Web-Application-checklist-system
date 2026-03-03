a51b0538f503ee8e097c5f5d7443f70130a7308c
artem <artemcherepanov551@gmail.com>
2026-03-03 23:01:10 +0500

    fix: archive

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -3,15 +3,12 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.TimeZone;
-
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Yekaterinburg"));
-        SpringApplication.run(BackendApplication.class, args);
+
 
 	}
 
